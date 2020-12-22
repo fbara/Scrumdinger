d7fae78a1147e27b9fc6fa9db23d1b46f3c7300d
fbara <fbara1@yahoo.com>
2020-12-22 12:19:40 -0600

    Creating a CardView.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -28,6 +28,9 @@ struct MeetingView: View {
                 }
                                 
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(Text("Time remaining"))
+            .accessibilityValue(Text("10 minutes"))
             
             Circle()
                 .strokeBorder(lineWidth: 24, antialiased: true)
@@ -38,6 +41,7 @@ struct MeetingView: View {
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel(Text("Next speaker"))
             }
             
         }

045b0d46568868e713eefaccd8cc42b9bf61f262
fbara <fbara1@yahoo.com>
2021-01-29 19:08:47 -0600

    Starting Persisting Data

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -46,6 +46,19 @@ struct DetailView: View {
                         .accessibilityValue(Text(attendee))
                 }
             }
+            
+            Section(header: Text("History")) {
+                if scrum.history.isEmpty {
+                    Label("No meeting yet.", systemImage: "calendar.badge.exclamationmark")
+                }
+                
+                ForEach(scrum.history) { history in
+                    HStack {
+                        Image(systemName: "calendar")
+                        Text(history.date, style: .date)
+                    }
+                }
+            }
         }
         .listStyle(InsetGroupedListStyle())
         .navigationBarItems(trailing: Button("Edit") {
